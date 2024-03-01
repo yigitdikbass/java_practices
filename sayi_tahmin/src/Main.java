d2039a11b47d17a76c4b7d167f41e2e216d17caa
yigitdikbass <153829751+yigitdikbass@users.noreply.github.com>
2024-03-01 11:29:06 +0300

    Add files via upload

---
+++ sayi_tahmin/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String args[]){
+        int[] dizi = new int[]{1,3,4,5,6,7,8,9};
+        int aranacak = 7;
+        if(SayiBul(dizi,aranacak)){
+            System.out.println("Sayı Dizide Bulundu");
+        }else{
+            System.out.println("Sayı Dizide Bulunamadı");
+        }
+    }
+    public static boolean SayiBul(int[] dizi, int aranacak){
+        boolean varmi = false;
+        for(int i = 0; i < dizi.length; i++){
+            if(dizi[i] == aranacak){
+                varmi = true;
+                break;
+            }
+        }
+        return varmi;
+    }
+}
